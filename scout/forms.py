0dca3ff6eb5ac32ac06c5606409dd49a8752333c
easti <ethan.astifan@gmail.com>
2019-12-01 21:21:21 -0500

    Made a forms class for each match, and a verification page

--- scout/forms.py
+++ scout/forms.py
@@ -7,6 +7,27 @@ class InfoForm(forms.Form):
     team = forms.ChoiceField(choices=[(t.id, t) for t in Team.objects.order_by('TeamNum')[:30]])
     round = forms.IntegerField(min_value=1, max_value=150)
 
+class SkystoneForm(forms.Form):
+    matchNum = forms.IntegerField()
+    position = forms.ChoiceField()
+    teamNum = forms.IntegerField()
+    # teamName = forms.CharField() # make autofillable from above
+
+    AUTO_StonesAcross = forms.IntegerField(max_value=6)
+    AUTO_StonesPlaced = forms.IntegerField(max_value=AUTO_StonesAcross)
+    AUTO_FirstSkystones = forms.IntegerField(max_value=2)
+    AUTO_Parking = forms.BooleanField()
+
+    DRIVER_StonesAcross = forms.IntegerField()
+    DRIVER_StonesPlaced = forms.IntegerField() # automatically include Auto placements
+    DRIVER_TallestStack = forms.IntegerField() # max_value=DRIVER_StonesPlaced
+
+    END_MarkerHeight = forms.IntegerField()
+    END_Parking = forms.BooleanField()
+    
+    POST_Comments = forms.Textarea()
+    POST_Breakage = forms.BooleanField()
+
 
 class MatchForm(forms.Form):
     Did_They_Run = forms.ChoiceField(choices=[(True, 'Yes'), (False, 'No')])
